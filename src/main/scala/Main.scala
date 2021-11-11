681b8ba91381bc04dce6c203301e480bd373a1ea
Kelsey Mok <kelseymok@gmail.com>
2021-11-11 08:50:20 +0100

    km: masking

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,28 +1,55 @@
 package org.scala.slurp
 
+import org.apache.spark.sql.DataFrame
 import software.amazon.awssdk.services.iam.IamClient
 import software.amazon.awssdk.services.s3.S3Client
 
 
 object Main extends App with SparkEnvironment {
 
-  val bucketName: String = "some-awesome-bucket"
+  val bucketName: String = ???
 
-  new S3BucketCreator(s3Client = S3Client.builder.build()).create(bucketName)
+  // HINT: We need a bucket to write to
+  /// HINT: initialise an S3 Client
+  val s3Client: S3Client = ???
+  /// HINT: Create a bucket using the s3Client and bucketName
+  ???
 
-  val df = new Ingester(spark).read()
-  val transformer = new DataTransformer(spark)
+  // HINT: Ingest data here
+  val df: DataFrame = ???
 
-  transformer.writeToS3Bucket(bucketName, "legendary", transformer.getLegendary(df))
-  transformer.writeToS3Bucket(bucketName, "nonLegendary", transformer.getNonLegendary(df))
+  // HINT: Initialise Transformer
+  val transformer: DataTransformer = ???
 
-  val roleName: String = "my-role"
-  val assumeRolePolicyDocument: String = ""
-  val accessorCreator = new AccessorCreator(iamClient = IamClient.builder().build())
-  accessorCreator.createRole(roleName, assumeRolePolicyDocument)
+  // HINT: Transform data
+  val legendaryPokemonDf: DataFrame = ???
+  val nonLegendaryPokemonDf: DataFrame = ???
 
-  val policyDocument: String = ""
-  val policyArn = accessorCreator.createPolicy(policyDocument)
+  // HINT: Write both legendary and nonLegendary to S3 Bucket with two different bucket paths
+  ???
+  ???
 
-  accessorCreator.attachPolicy(roleName = roleName, policyArn = policyArn)
+  // HINT: Define a role name
+  val roleName: String = ???
+
+  // HINT: Who can assume this role? Does scala have JSON -> String libraries?
+  val assumeRolePolicyDocument: String = ???
+
+  // HINT:
+  val iamClient: IamClient = ???
+
+  // HINT: Initialise AccessorCreator
+  val accessorCreator = new AccessorCreator(iamClient = iamClient)
+
+  // HINT: use the accessorCreator to create a role with the assumeRolePolicyDocument
+  ???
+
+  // HINT: what kind of actions will this role carry out. Avoid using only "*" in the actions and resources.
+  val policyDocument: String = ???
+
+  // HINT: use the accessorCreator to create a policy using the policyDocument
+  val policyArn = ???
+
+  // HINT: use the accessorCreator to attach the policy to your role
+  ???
 }
